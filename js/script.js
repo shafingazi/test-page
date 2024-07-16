2cc5981e4a604dc9531817185720bd188735f6ae
shafingazi <34550119+shafingazi@users.noreply.github.com>
2024-07-16 01:37:15 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //Make the DIV element draggagle:
 dragElement(document.getElementById("mydiv"));
 
+swipeElement(document.getElementById("mydiv"));
+
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
@@ -39,5 +42,47 @@ function dragElement(elmnt) {
     /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
+  }
+}
+
+
+function swipeElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").ontouchstart = swipeTouchStart;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.ontouchstart = swipeTouchStart;
+  }
+
+  function swipeTouchStart(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.ontouchend = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.ontouchmove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.ontouchend = null;
+    document.ontouchmove = null;
   }
 }
